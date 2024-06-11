b137089c0ca28ee9b58ab0b7a1e475f40698934c
Ange_Tech(Angel_Molina) <166965796+AngelTech90@users.noreply.github.com>
2024-06-10 18:01:05 -0600

    Create of responsive nav bar and adding the footer with social media of the page

---
+++ scripts/aboutUsScript.js
@@ -0,0 +1,7 @@
+
+//*Here we'll show our menu access:
+if(document.getElementById('stackedMenuAccess').checked == true){
+
+    document.getElementById('optionContainer2').style.display = 'flex';
+
+}
